71b238263fa7924381f20644a4fffc926abc341e
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-01 16:59:46 +0700

    StartThread was added

--- saya/__init__.py
+++ saya/__init__.py
@@ -2,10 +2,11 @@
 # author: Ethosa
 
 from .VK.Vk import Vk
+from .VK.Event import Event
 
 __copyright__ = "2020"
 __version__ = "0.0.1"
 __authors__ = ["Ethosa"]
 
 if __name__ == '__main__':
-    print(Vk)
+    print(Vk, Event)
