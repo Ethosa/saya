8d06a5e386fb55d783224b142ed9bf8d282fbf22
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-02 07:57:23 +0700

    м0ю0ю4

--- saya/__init__.py
+++ saya/__init__.py
@@ -2,7 +2,7 @@
 # author: Ethosa
 
 from .VK import (Vk, LongPoll, Event, PushEvent, VkAuthManager,
-                 VkScript, Uploader, Button, Keyboard)
+                 VkScript, Uploader, Button, Keyboard, TemplateElement, Template)
 
 __copyright__ = "2020"
 __version__ = "0.0.4"
@@ -11,4 +11,4 @@ __authors__ = ["Ethosa"]
 if __name__ == '__main__':
     print(Vk, LongPoll, VkAuthManager, Event,
           PushEvent, VkScript, Uploader, Button,
-          Keyboard)
+          Keyboard, TemplateElement, Template)
