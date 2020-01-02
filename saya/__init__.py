ab46bbef774262b3cd6f087d7de53bfd8f72fe22
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-02 08:12:32 +0700

    code fix

--- saya/__init__.py
+++ saya/__init__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # author: Ethosa
 
-from .VK import (Vk, LongPoll, Event, PushEvent, VkAuthManager,
-                 VkScript, Uploader, Button, Keyboard, TemplateElement, Template)
+from .VK import (Vk, LongPoll, Event, VkAuthManager, VkScript,
+                 Uploader, Button, Keyboard, TemplateElement,
+                 Template)
 
 __copyright__ = "2020"
 __version__ = "0.0.4"
 __authors__ = ["Ethosa"]
 
 if __name__ == '__main__':
-    print(Vk, LongPoll, VkAuthManager, Event,
-          PushEvent, VkScript, Uploader, Button,
-          Keyboard, TemplateElement, Template)
+    print(Vk, LongPoll, VkAuthManager, Event, VkScript,
+          Uploader, Button, Keyboard, TemplateElement,
+          Template)
