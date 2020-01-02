72124d155d12e200ed1e2da623b29cff7f4eaa49
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-02 08:55:26 +0700

    VkScript.py - added new rules

--- saya/VK/VkScript.py
+++ saya/VK/VkScript.py
@@ -47,7 +47,7 @@ class VkScript(Translator):
         # if (...){
         #     ...
         # }
-        ((r"(?P<enter>[\r\n]+)(?P<indent>[ ]*)(?P<block>if|while|for|elif)[ ]*"
+        ((r"(?P<enter>[\r\n]+)(?P<indent>[ ]*)(?P<block>if|while|elif)[ ]*"
           r"(?P<block_info>[^:{}]+):"
           r"(?P<body>[\r\n]+(?P<block_indent>(?P=indent)[ ]*)"
           r"[^\r\n]+[\r\n]+((?P=block_indent)[^\r\n]+[\r\n]+)*)"),
@@ -67,6 +67,58 @@ class VkScript(Translator):
          (r"\g<enter>\g<indent>else\g<block_info>{\g<body>\g<indent>}\n"),
          None, 70),
 
+        # for i in range(10):
+        #   ...
+        # ------------------
+        # i = 0
+        # while i < 10:
+        #   ...
+        # i = i+1
+        ((r"(?P<enter>[\r\n]+)(?P<indent>[ ]*)for[ ]*"
+          r"(?P<var>[a-zA-Z0-9_]+)[ ]*in[ ]*range[ ]*\((?P<end>[^,\)]+)\):"
+          r"(?P<body>[\r\n]+(?P<block_indent>(?P=indent)[ ]*)"
+          r"[^\r\n]+[\r\n]+((?P=block_indent)[^\r\n]+[\r\n]+)*)"),
+         (r"\g<enter>\g<indent>var \g<var> = 0;\n"
+          r"\g<indent>while (\g<var> < \g<end>){"
+          r"\g<body>\g<block_indent>\g<var> = \g<var> + 1;\n"
+          r"\g<indent>}\n"),
+         None, 70),
+
+        # for i in range(0, 10):
+        #   ...
+        # ------------------
+        # i = 0
+        # while i < 10:
+        #   ...
+        # i = i+1
+        ((r"(?P<enter>[\r\n]+)(?P<indent>[ ]*)for[ ]*"
+          r"(?P<var>[a-zA-Z0-9_]+)[ ]*in[ ]*range[ ]*\((?P<start>[^,]+),[ ]*(?P<end>[^,\)]+)\):"
+          r"(?P<body>[\r\n]+(?P<block_indent>(?P=indent)[ ]*)"
+          r"[^\r\n]+[\r\n]+((?P=block_indent)[^\r\n]+[\r\n]+)*)"),
+         (r"\g<enter>\g<indent>var \g<var> = \g<start>;\n"
+          r"\g<indent>while (\g<var> < \g<end>){"
+          r"\g<body>\g<block_indent>\g<var> = \g<var> + 1;\n"
+          r"\g<indent>}\n"),
+         None, 70),
+
+        # for i in range(0, 10, 2):
+        #   ...
+        # ------------------
+        # i = 0
+        # while i < 10:
+        #   ...
+        # i = i+2
+        ((r"(?P<enter>[\r\n]+)(?P<indent>[ ]*)for[ ]*"
+          r"(?P<var>[a-zA-Z0-9_]+)[ ]*in[ ]*range[ ]*\((?P<start>[^,]+),[ ]*"
+          r"(?P<end>[^,]+),[ ]*(?P<step>[^,\)]+)\):"
+          r"(?P<body>[\r\n]+(?P<block_indent>(?P=indent)[ ]*)"
+          r"[^\r\n]+[\r\n]+((?P=block_indent)[^\r\n]+[\r\n]+)*)"),
+         (r"\g<enter>\g<indent>var \g<var> = \g<start>;\n"
+          r"\g<indent>while (\g<var> < \g<end>){"
+          r"\g<body>\g<block_indent>\g<var> = \g<var> + \g<step>;\n"
+          r"\g<indent>}\n"),
+         None, 70),
+
         # pass
         #
         ((r"pass"),
@@ -75,7 +127,7 @@ class VkScript(Translator):
 
         # a = 10
         # a = 10;
-        ((r"([\r\n]*)([ ]*)([^#])([\S ]+[^;{}])\n"),
+        ((r"([\r\n]*)([ ]*)([^/]{2})([\S ]+[^;{}])\n"),
          (r"\1\2\3\4;\n"),
          None, 0),
 
