b3286ea8013e49a80826683962a5359dbde71772
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-02 07:32:33 +0700

    Keyboard and Button was added

--- saya/VK/__init__.py
+++ saya/VK/__init__.py
@@ -7,6 +7,11 @@ from .VkAuthManager import VkAuthManager
 from .Event import Event
 from .PushEvent import PushEvent
 from .VkScript import VkScript
+from .Uploader import Uploader
+from .Button import Button
+from .Keyboard import Keyboard
 
 if __name__ == '__main__':
-    print(Vk, LongPoll, VkAuthManager, Event, PushEvent, VkScript)
+    print(Vk, LongPoll, VkAuthManager, Event,
+          PushEvent, VkScript, Uploader, Button,
+          Keyboard)
