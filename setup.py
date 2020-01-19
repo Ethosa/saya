a482dfed48da4da19b6353cab845f8dc8b8e25ed
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-19 20:19:07 +0700

    v0.1.43

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saya",
-    version="0.1.42",
+    version="0.1.43",
     author="Ethosa",
     author_email="social.ethosa@gmail.com",
     description="The framework for vk",
