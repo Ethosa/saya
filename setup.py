f325b2e0ad5bc388a3b3ea99beedc07a80effc05
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-24 17:57:43 +0700

    v0.1.5, added Vk.to_execute method

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saya",
-    version="0.1.47",
+    version="0.1.5",
     author="Ethosa",
     author_email="social.ethosa@gmail.com",
     description="The framework for vk",
