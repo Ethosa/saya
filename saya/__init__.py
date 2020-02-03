fc1e5107313083cfe9137568a822155870bf4660
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-02-04 00:43:09 +0700

    v0.2.2
    The Button class has been replaced with the create_button function.

--- saya/__init__.py
+++ saya/__init__.py
@@ -2,16 +2,16 @@
 # author: Ethosa
 
 from .VK import (Vk, LongPoll, event, VkAuthManager, VkScript,
-                 Uploader, Button, Keyboard, TemplateElement,
+                 Uploader, create_button, Keyboard, TemplateElement,
                  Template, StreamingAPI)
 from .AVK import (AVk, ALongPoll)
 
 __copyright__ = "2020"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __authors__ = ["Ethosa"]
 
 if __name__ == '__main__':
     print(Vk, LongPoll, VkAuthManager, event, VkScript,
-          Uploader, Button, Keyboard, TemplateElement,
+          Uploader, create_button, Keyboard, TemplateElement,
           Template, StreamingAPI)
     print(AVk, ALongPoll)
