155a53d7bafff6c8d6dd166f81a80421c1f98f46
SakiKawasaki <theniklick@gmail.com>
2020-04-18 03:46:51 +0700

    update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saya",
-    version="0.3.1",
+    version="0.3.5",
     author="Ethosa",
     author_email="social.ethosa@gmail.com",
     description="The framework for vk",
