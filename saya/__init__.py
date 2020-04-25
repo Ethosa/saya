6ad482be0c1cdeee6066362d0058ab5e5113d13f
SakiKawasaki <theniklick@gmail.com>
2020-04-25 17:26:04 +0700

    update version.

--- saya/__init__.py
+++ saya/__init__.py
@@ -7,7 +7,7 @@ from .VK import (Vk, LongPoll, event, VkAuthManager, VkScript,
 from .AVK import (AVk, ALongPoll, AUploader, AStreamingAPI)
 
 __copyright__ = "2020"
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 __authors__ = ["Ethosa"]
 
 if __name__ == '__main__':
